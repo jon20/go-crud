9bf1e13ff1a4f1570bc86a0e93035fa61e85c9ba
jon20 <kawadu1221@gmail.com>
2018-05-05 19:54:17 +0900

    first

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"log"
+	"fmt"
+)
+
+type Res_Hello struct {
+	Messsage string `json:"message"`
+	}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", apiRequest)
+	log.Fatal(http.ListenAndServe(":9000", mux))
+}
+
+func apiRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	
+	res, _ := json.Marshal(Res_Hello{"Hello World!!"})
+	fmt.Fprintf(w, string(res))
+	fmt.Println("Hello")
+}
